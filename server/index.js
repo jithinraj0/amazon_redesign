2b7d8124041fa9f613c5b666c10fd12481062192
Jithin Raj <jithin6904@gmail.com>
2022-07-18 11:14:44 -0400

    integrating nodejs

--- server/index.js
+++ server/index.js
@@ -1,18 +1,15 @@
-console.log("Hello World!!");
 
 const express = require('express');
-
+const authRouter = require('./routes/auth')
 const PORT = 3000;
 
 const app = express();
 
-app.listen(PORT, "0.0.0.0", () => {
+app.use(authRouter);
+
+app.listen(PORT, () => {
     console.log(`connected at ${PORT} hiii.`);
-})
+});
 
-//API operations
 
-app.get("/hello-world", (req, res) => {
-    res.send("hello world");
-})
 
---
+++ server/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+
+const authRouter = express.Router();
+
+authRouter.get("/user", (req, res) => {
+    res.json({ msg: "rivaan" });
+});
+
+
+
+module.exports = authRouter;
